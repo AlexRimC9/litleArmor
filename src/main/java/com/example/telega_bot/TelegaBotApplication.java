36e8912fcfe2aca2b0f58ad8a26021c56e542efa
Bober <aliancewin704@gmail.com>
2022-03-07 12:35:02 +0300

    Start version 0.00

---
+++ src/main/java/com/example/telega_bot/TelegaBotApplication.java
@@ -0,0 +1,20 @@
+package com.example.telega_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegaBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegaBotApplication.class, args);
+    }
+
+    //1 git
+    //2 Мини армор геймс{}
+    //3 чат + френд лист
+    //4 таблица рекордов + ачивки
+    //5 невс тбл
+    //6 попробуем сетка для нескольких пользователей
+
+}
